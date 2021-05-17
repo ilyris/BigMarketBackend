2c4a00d61966e93c6922731762cb03731f840d8d
Dylan <dylan.sieren01@gmail.com>
2021-05-17 00:38:28 -0500

    DTS: added knex/pg updated scripts

--- src/routes/productListing.js
+++ src/routes/productListing.js
@@ -6,10 +6,7 @@ router.post("/", async (req, res, next) => {
     // const { profileId } = req.body
     console.log(req.body);
     try {
-        // Make a SQL request on the column 'email' with the value in the variable 'emailAddr'
-        // const usersProfileData = await findProfileInformation({ id: profileId }) // create a inner join to get profile data for a user based off ID
-        // // Json the object we get back.
-        // res.json({ usersProfileData })
+
     } catch (error) {
         console.log(error)
         next(error)
@@ -17,7 +14,7 @@ router.post("/", async (req, res, next) => {
 })
 router.get("/", async (req, res, next) => {
     // const { profileId } = req.body
-    console.log(req.body);
+    console.log(req.body);  
     try {
         // Make a SQL request on the column 'email' with the value in the variable 'emailAddr'
         // const usersProfileData = await findProfileInformation({ id: profileId }) // create a inner join to get profile data for a user based off ID
