25fa47fdf8a89323df3c74a929cdf923c510fb34
Dylan <dylan.sieren01@gmail.com>
2021-05-18 23:52:37 -0500

    changed username to name in the User migration schema

--- data/migrations/20210517000138_User.js
+++ data/migrations/20210517000138_User.js
@@ -1,7 +1,7 @@
 exports.up = (knex) =>
     knex.schema.createTable("User", (tbl) => {
         tbl.increments("id").primary()
-        tbl.string("username", 128).notNullable().unique()
+        tbl.string("name", 128).notNullable().unique()
         tbl.string("email", 128).notNullable().unique()
         tbl.string("password", 128).notNullable()
         tbl.timestamps(true, true)
